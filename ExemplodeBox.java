5e57713db8143d75789a31ac681ea966eb49a40b
binaa02 <129764920+binaa02@users.noreply.github.com>
2023-04-06 14:14:38 -0300

    Create ExemplodeBox.java

---
+++ ExemplodeBox.java
@@ -0,0 +1,9 @@
+package Documents.repos.profdiegoaugusto;
+
+public class ExemplodeBox {
+}
+        System.out.println("┌────────────┐");
+        System.out.println("│            │");
+        System.out.println("│  Conteúdo  │");
+        System.out.println("│            │");
+        System.out.println("└────────────┘");

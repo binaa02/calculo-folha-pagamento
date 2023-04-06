ddf47d190a9b58756d224009039cfcb14928e621
binaa02 <129764920+binaa02@users.noreply.github.com>
2023-04-06 13:10:17 -0300

    Create CalcularPericulosidade.java

---
+++ CalcularPericulosidade.java
@@ -0,0 +1,9 @@
+package CalcularPericulosidade;
+    public class PericulosidadeCalculator {
+
+        public double calcularAdicionalPericulosidade(double salario) {
+            double adicional = salario * 0.3;
+            return adicional;
+        }
+
+    }
